0d27fb5e5b6685a68d8a038e45e08e8344c3d736
Felicia Powell <soccerkick7@felicia-powells-macbook-pro.local>
2013-03-21 00:30:54 -0400

    new arduino file with some example code that makes the yellow and white better using PWM digital pins

---
+++ NYT_Serial/RGB_blink/RGB_blink.ino
@@ -0,0 +1,34 @@
+int redPin = 3;
+int greenPin = 5;
+int bluePin = 6;
+
+void setup(){
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+}
+
+void loop ()
+  {
+    setColor(255, 0, 0); //red
+    delay(1000);
+    setColor(0, 255, 0); //green
+    delay(1000);
+    setColor(0, 0, 255); //blue
+    delay(1000);
+    setColor(255, 80, 0); //yellow
+    delay(1000);
+    setColor(200, 0, 255); //purple
+    delay(1000);
+    setColor(0,255, 100); // aqua
+    delay(1000);
+    setColor(255,100,100); //white
+    delay(1000);
+  }
+
+void setColor(int red, int green, int blue)
+{
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);
+}
